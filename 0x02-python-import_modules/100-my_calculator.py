02c4e49938633d95cc4e0462b58317ce7864761d
Kroser-draid <kingdomskroser@gmail.com>
2024-05-02 04:31:27 +0100

    fixation

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,7 +2,6 @@
 if __name__ == "__main__":
     from calculator_1 import add, mul, sub, div
     from sys import argv
-    
     if (len(argv) - 1) < 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
