69947c6b56063e4d9282da3d0ece08d54b04d568
Kroser-draid <kingdomskroser@gmail.com>
2024-05-10 16:04:05 +0100

    fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,6 +4,6 @@ def print_matrix_integer(matrix=[[]]):
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
                 print("{}".format(matrix[i][j]), end=" ")
-            print()
+            print("{}".format(""))
     else:
-        print()
+        print("{}".format(""))
