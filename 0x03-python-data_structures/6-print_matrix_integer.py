2056f6ce261a2864dcbda7f3c585fff1abb0eab4
Kroser-draid <kingdomskroser@gmail.com>
2024-05-10 16:15:37 +0100

    fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,10 @@ def print_matrix_integer(matrix=[[]]):
     if len(matrix) > 1:
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
-                print("{:d}".format(matrix[i][j]), end=" ")
+                if j < len(matrix[i]):
+                    print("{:d}".format(matrix[i][j]), end=" ")
+                else:
+                    print("{:d}".format(matrix[i][j]))
             print("{}".format(""))
     else:
         print("{}".format(""))
