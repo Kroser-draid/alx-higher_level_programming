fe408c9d32881fba2b7aa68eb456573bfc2ce659
Kroser-draid <kingdomskroser@gmail.com>
2024-05-13 19:43:37 +0100

    fic

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     h_score = 0
-    h_score_win = ''
-    if not a_dictionary and a_dictionary == {}:
+    h_score_win = 'ayoub'
+    if a_dictionary is not None and a_dictionary != {}:
         for i in a_dictionary:
             if a_dictionary[i] > h_score:
                 h_score = a_dictionary[i]
