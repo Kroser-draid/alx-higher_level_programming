c0edb3305451606f086b5bebe3e5016fc37fccaa
Kroser-draid <kingdomskroser@gmail.com>
2024-05-14 13:53:47 +0100

    fix 2

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,6 +6,7 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except (TypeError, ValueError) as e_message:
-        error_msg = "Exception: " + str(e_message)
-        sys.stderr.write(error_msg)
+        message = "Exception: " + str(e_message)
+        sys.stderr.write(message)
         return False
+    return True
