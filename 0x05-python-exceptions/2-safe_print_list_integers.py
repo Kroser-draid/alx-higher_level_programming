76d91356f43159b112d3b8f7badfe7d6a341d74f
Kroser-draid <kingdomskroser@gmail.com>
2024-05-24 20:42:10 +0100

    raising

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,9 +3,11 @@ def safe_print_list_integers(my_list=[], x=0):
     result = 0
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i]), end='')
+            print("{:d}".format(my_list[i]), end="")
             result += 1
         except (ValueError, TypeError):
-            continue
-    print('')
+            pass
+        except IndexError:
+            raise
+    print("")
     return result
