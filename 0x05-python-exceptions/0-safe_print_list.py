752aa42c662c022330e43ab68623909d3c95807b
Kroser-draid <kingdomskroser@gmail.com>
2024-05-24 20:14:59 +0100

    change smthg

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     result = 0
-    length = 0
     try:
-        for i in my_list:
-            length += 1
-    except IndexError:
-        pass
-    for j in range(length):
-        if j < x:
-            print(my_list[j], end='')
+        for i in range(x):
+            print(my_list[i], end="")
             result += 1
-        else:
-            break
-    print('')
+    except Exception:
+        pass
+    print("")
     return result
