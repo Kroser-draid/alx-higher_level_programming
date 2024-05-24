b45375d2af48b9355ce26e9329833421de239f01
Kroser-draid <kingdomskroser@gmail.com>
2024-05-25 00:55:00 +0100

    Task 8

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 import sys
-
-
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-    except (TypeError, ValueError) as e_message:
-        sys.stderr.write("Exception: {}\n".format(e_message))
-        return False
-    else:
         return True
+    except Exception as e:
+        message = "Exception: {}\n".format(e)
+        sys.stderr.write(message)
+        return False
