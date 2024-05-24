a9a809e890c815b3ee8aacb0881836b0436e644e
Kroser-draid <kingdomskroser@gmail.com>
2024-05-24 02:57:22 +0100

    fix 6th task

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 -- script that show all descriptions of a table
--- selecting all descriptions from the table
-SELECT * FROM first_table;
+-- selecting all descriptions from the TABLE
+SHOW COLUMNS FROM first_table;
