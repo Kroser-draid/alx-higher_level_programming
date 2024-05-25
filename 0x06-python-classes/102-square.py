c68c619201fab52bd910f804fb2126495c15c2e1
Kroser-draid <kingdomskroser@gmail.com>
2024-05-25 03:57:14 +0100

    square 102

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,76 +1,64 @@
 #!/usr/bin/python3
-"""square class"""
+"""
+square class
+"""
 
 
 class Square:
-    """square class"""
+    """
+    square class
+    """
     def __init__(self, size=0):
-        """init square
-        Args:
-            size (int): square size.
-        """
         self.__size = size
+
+    def area(self):
+        return self.__size ** 2
 
     @property
     def size(self):
-        """get value attribute.
-
-        Returns:
-            int: the attribute value.
-        """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Sets value into size, must be int.
-
-        Args:
-            value (int): square size.
-
-        Raises:
-            TypeError: if not int.
-            ValueError: if not positive.
-        """
-        if not isinstance(value, (int, float)):
-            raise TypeError("size must be a number")
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
 
-    def area(self):
-        """calc area
-        returns:
-            return area.
-        """
-        return self.__size * self.__size
+    def __eq__(self, another_square):
+        if self.area() == another_square.area():
+            return True
+        else:
+            return False
 
-    def __eq__(self, other):
-        """equality comparison =="""
-        if isinstance(other, Square):
-            return self.__size == other.__size
-        return False
+    def __lt__(self, another_square):
+        if self.area() < another_square.area():
+            return True
+        else:
+            return False
 
-    def __ne__(self, other):
-        """not equal comparison !="""
-        return not self.__eq__(other)
+    def __le__(self, another_square):
+        if self.area() <= another_square.area():
+            return True
+        else:
+            return False
 
-    def __lt__(self, other):
-        """less than comparison <"""
-        if isinstance(other, Square):
-            return self.__size < other.__size
-        return NotImplemented
+    def __gt__(self, another_square):
+        if self.area() > another_square.area():
+            return True
+        else:
+            return False
 
-    def __le__(self, other):
-        """less equal comparison <="""
-        return self.__lt__(other) or self.__eq__(other)
+    def __ge__(self, another_square):
+        if self.area() >= another_square.area():
+            return True
+        else:
+            return False
 
-    def __gt__(self, other):
-        """greater than comparison >"""
-        if isinstance(other, Square):
-            return self.__size > other.__size
-        return NotImplemented
-
-    def __ge__(self, other):
-        """greater equal comparison >="""
-        return self.__gt__(other) or self.__eq__(other)
+        def __ne__(self, another_square):
+            if self.area() != another_square.area():
+                return True
+            else:
+                return False
