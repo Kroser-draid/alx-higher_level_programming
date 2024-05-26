f25825ad63b78e1215fe544f374b266953c362e6
Kroser-draid <kingdomskroser@gmail.com>
2024-05-26 21:24:04 +0100

    test 2

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,41 +1,34 @@
 #!/usr/bin/python3
-"""matrix_divided."""
+"""
+doc
+"""
 
 
 def matrix_divided(matrix, div):
-    """matrix_divided.
+    if not isinstance(matrix, list):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    elif len(matrix) < 2:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    else:
+        first_len = len(matrix[0])
+        for i in range(len(matrix)):
+            if len(matrix[i]) != first_len:
+                raise TypeError("Each row of the matrix must have the same size")
+            for j in range(len(matrix[i])):
+                    if not isinstance(matrix[i][j], (int, float)):
+                        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
-    Args:
-        param matrix: the matrix to e divided.
-        param div: the divisor.
-    Returns:
-        return a new matrix.
-    """
-    type_msg = "matrix must be a matrix (list of lists) of integers/floats"
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
-
-    if div == 0:
+    elif div == 0:
         raise ZeroDivisionError("division by zero")
-
-    if not isinstance(matrix, list) or not matrix:
-        raise TypeError(type_msg)
-
-    if not all(isinstance(item, list) and item for item in matrix):
-        raise TypeError(type_msg)
-
-    if not all(isinstance(num, (int, float)) for row in matrix for num in row):
-        raise TypeError(type_msg)
-
-    row_len = len(matrix[0])
-    if not all(len(row) == row_len for row in matrix):
-        raise TypeError("Each row of the matrix must have the same size")
+    
 
     new_matrix = []
-    for row in matrix:
-        new_row = []
-        for num in row:
-            result = round(num / div, 2)
-            new_row.append(result)
-        new_matrix.append(new_row)
+    for i in range(len(matrix)):
+        new_matrix.append([])
+        for j in range(len(matrix[i])):
+            new_matrix[i].append(round((matrix[i][j] / div), 2))
+
     return new_matrix
+
