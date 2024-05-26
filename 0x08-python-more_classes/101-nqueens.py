ed85f93847d2a045ab93a1ea55aaf80477b7bbd5
Kroser-draid <kingdomskroser@gmail.com>
2024-05-26 01:49:37 +0100

    sending it to debugger

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,26 +1,48 @@
 #!/usr/bin/python3
 import sys
-if len(sys.argv) > 2 or len(sys.argv) == 1:
+
+
+if len(sys.argv) != 2:
     print("Usage: nqueens N")
-    exit(1)
+    sys.exit(1)
 else:
     try:
-        arg = int(sys.argv[1])
-    except ValueError:
+        if int(sys.argv[1]) < 4:
+            print("N must be at least 4")
+            sys.exit(1)
+    except Exception:
         print("N must be a number")
-        exit(1)
-    if arg < 4:
-        print("N must be at least 4")
-        exit(1)
-    else:
-        #solutions = []
-        solution = []
-        prev = 0
-        prev_i = -1
-        for i in range(arg):
-            for j in range(arg):
-                if (j != i) and (j != prev + 1) and (j != prev - 1) and (i != prev_i) and (j not in solution):
-                    solution.append((i, j))
-                    prev = j
-                    prev_i = i
-        print(solution)
+        sys.exit(1)
+
+columns = int(sys.argv[1])
+rows = columns
+prev_q = [-1, -2]
+prev_q_col = []
+solutions = []
+all_solutions = []
+prev_q_rows = []
+column = 0
+sol = 0
+for sol in range(rows):
+    while column < columns:
+        row = 0
+        indice = 0
+        while row < rows:
+            if (column not in prev_q_col) and ((row != prev_q[1] + 1) and (row != prev_q[1] - 1)) and ((row != column) and (row not in prev_q_rows)):
+                solutions.append([column, row])
+                prev_q[0] = column
+                prev_q[1] = row
+                row += 1
+                prev_q_col.append(column)
+                prev_q_rows.append(row)
+                indice = 1
+                column += 1
+            else:
+                row += 1
+        if indice == 0:
+            column += 1
+    all_solutions.append(solutions)
+str_solution = str(all_solutions).replace('[[[', ' [[')
+str_solution = str_solution.replace(']]]', ']]')
+str_solution = str_solution.replace(']],', ']]\n')
+print(str_solution)
