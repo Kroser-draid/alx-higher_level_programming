1aa02a79a93214d0f3c7460baf5876a07e0a4310
Kroser-draid <kingdomskroser@gmail.com>
2024-05-26 21:31:27 +0100

    fix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -5,24 +5,26 @@ doc
 
 
 def matrix_divided(matrix, div):
+    int_float = "matrix must be a matrix (list of lists) of integers/floats"
+    message_1 = "Each row of the matrix must have the same size"
+    message_2 = "matrix must be a matrix (list of lists) of integers/floats"
     if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(int_float)
     elif len(matrix) < 2:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(int_float)
     else:
         first_len = len(matrix[0])
         for i in range(len(matrix)):
             if len(matrix[i]) != first_len:
-                raise TypeError("Each row of the matrix must have the same size")
+                raise TypeError(message_1)
             for j in range(len(matrix[i])):
-                    if not isinstance(matrix[i][j], (int, float)):
-                        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                if not isinstance(matrix[i][j], (int, float)):
+                    raise TypeError(message_2)
 
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     elif div == 0:
         raise ZeroDivisionError("division by zero")
-    
 
     new_matrix = []
     for i in range(len(matrix)):
@@ -31,4 +33,3 @@ def matrix_divided(matrix, div):
             new_matrix[i].append(round((matrix[i][j] / div), 2))
 
     return new_matrix
-
