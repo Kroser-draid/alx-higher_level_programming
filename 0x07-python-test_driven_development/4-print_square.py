9887e5ed44fbd8dcf874b12b9f83cbf94edf5135
Kroser-draid <kingdomskroser@gmail.com>
2024-05-27 03:51:55 +0100

    task 4

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-"""print_square."""
+"""
+print square Module
+"""
 
 
 def print_square(size):
-    """print_square.
-    Args:
-        param size: square size.
     """
-    if isinstance(size, float) and size < 0:
-        raise TypeError("size must be an integer")
-
+    print square of #
+    """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
 
     if size < 0:
         raise ValueError("size must be >= 0")
 
-    square_pattern = "#" * size + "\n"
-    print(square_pattern * size, end="")
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print("")
