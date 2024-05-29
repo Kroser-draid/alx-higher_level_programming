59359060e6449e4849632f6b95d602ef388b3cdf
Kroser-draid <kingdomskroser@gmail.com>
2024-05-29 17:41:10 +0100

    fix

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -8,7 +8,7 @@ def is_same_class(obj, a_class):
     """
     function that check if it's instance of class
     """
-    if type(obj) == a_class:
+    if type(obj) is a_class:
         return True
     else:
         return False
