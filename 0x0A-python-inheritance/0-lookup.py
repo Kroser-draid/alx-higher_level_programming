7dc97cb80eab3820fdc7b5d0b578d224a44d96db
Kroser-draid <kingdomskroser@gmail.com>
2024-06-15 05:58:09 +0100

    task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
-"""
-Module of function
-"""
-
-
 def lookup(obj):
-    """
-    function that return list
-    """
-    new_list = []
-    new_list = dir(obj)
-    return new_list
+    return obj.__dict__
