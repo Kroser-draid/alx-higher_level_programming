b9ea59585212fb7142fe94065ea004092d5bca96
Kroser-draid <kingdomskroser@gmail.com>
2024-06-15 05:30:55 +0100

    task 29

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
     magic_string.call_count = getattr(magic_string, 'call_count', 0) + 1
-    return ("Best School, " * (magic_string.call_count - 1) + "Best School")
+    return ("BestSchool, " * (magic_string.call_count - 1) + "BestSchool")
