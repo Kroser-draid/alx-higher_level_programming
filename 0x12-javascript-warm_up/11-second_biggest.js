5ad8816c69bb08081814df9809662e3bbb9c3f6a
Kroser-draid <kingdomskroser@gmail.com>
2024-07-04 05:08:23 +0100

    task 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  const number = [];
+  const length = process.argv.length;
+  for (let i = 2; i < length; i++) {
+    number.push(Number(process.argv[i]));
+  }
+  number.sort();
+  console.log(number[number.length - 2]);
+}
