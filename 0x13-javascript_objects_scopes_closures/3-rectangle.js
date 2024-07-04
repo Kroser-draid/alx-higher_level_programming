2135141ac94a8142a8db3e761dd545a01c08a6ab
Kroser-draid <kingdomskroser@gmail.com>
2024-07-04 14:25:26 +0100

    fix 1

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -11,7 +11,7 @@ class Rectangle {
   print () {
     let message = '';
     for (let j = 0; j < (this.width); j++) {
-      message += 'x';
+      message += 'X';
     }
     for (let i = 0; i < (this.height); i++) {
       console.log(message);
