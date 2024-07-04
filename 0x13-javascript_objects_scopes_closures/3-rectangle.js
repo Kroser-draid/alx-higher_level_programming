ff7e2916f34557e6330f3b94076e755ae3c1e9f3
Kroser-draid <kingdomskroser@gmail.com>
2024-07-04 15:31:36 +0100

    .

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -2,19 +2,19 @@
 
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (h > 0 && !Number.isNaN(h) && w > 0 && !Number.isNaN(w)) {
-      this.height = h;
+    if (w > 0 && !Number.isNaN(w) && h > 0 && !Number.isNaN(h)) {
       this.width = w;
+      this.height = h;
     }
   }
 
   print () {
     for (let i = 0; i < this.height; i++) {
-      let message = '';
+      let result = '';
       for (let j = 0; j < this.width; j++) {
-        message += 'X';
+        result += 'X';
       }
-      console.log(message);
+      console.log(result);
     }
   }
 };
