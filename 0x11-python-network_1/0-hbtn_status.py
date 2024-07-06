3805dc3b552a07f3c6c3489c07c199e219fefc21
Kroser-draid <kingdomskroser@gmail.com>
2024-07-06 16:58:40 +0100

    fix task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,7 @@ url = "https://alx-intranet.hbtn.io/status"
 
 with urllib.request.urlopen(url) as response:
     data = response.read()
+print("Body response:")
 print("- type: ", type(data))
 print("- content:", data)
 print("- utf8 content:", data.decode("utf-8"))
