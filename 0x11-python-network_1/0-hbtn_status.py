37fdc2b54d2ad1db1f4991e231167115726c77c4
Kroser-draid <kingdomskroser@gmail.com>
2024-07-06 16:53:21 +0100

    task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# script that retrieve data from response
+import urllib
+from urllib import request
+
+url = "https://alx-intranet.hbtn.io/status"
+
+with urllib.request.urlopen(url) as response:
+    data = response.read()
+print("- type: ", type(data))
+print("- content:", data)
+print("- utf8 content:", data.decode("utf-8"))
