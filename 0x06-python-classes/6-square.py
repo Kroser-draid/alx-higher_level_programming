cdaf8cd0f782ff308933d817950192351b158f83
Kroser-draid <kingdomskroser@gmail.com>
2024-07-09 06:43:03 +0100

    rev 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,54 +1,30 @@
 #!/usr/bin/python3
 """
-class Square
+Module of a square
 """
 
 
 class Square:
     """
-    class square
+    class of square
     """
     def __init__(self, size=0, position=(0, 0)):
         if not isinstance(size, int):
-            raise TypeError("size must be an integer")
+            raise TypeError('size must be an integer')
         elif size < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError('size must be >= 0')
         else:
             self.__size = size
         if not isinstance(position, tuple):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif len(position) != 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
+            raise TypeError('position must be a tuple of 2 positive integers')
         elif not isinstance(position[0], int):
-            raise TypeError("position must be a tuple of 2 positive integers")
+            raise TypeError('position must be a tuple of 2 positive integers')
         elif not isinstance(position[1], int):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif (position[0] < 0 or position[1] < 0):
-            raise TypeError("position must be a tuple of 2 positive integers")
+            raise TypeError('position must be a tuple of 2 positive integers')
         else:
             self.__position = position
 
-    @property
-    def position(self):
-        return self.__position
-
-    @position.setter
-    def position(self, value):
-        if not isinstance(value, tuple):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif len(value) != 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif not (isinstance(value[0], int) and isinstance(value[1], int)):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif (value[0] < 0 or value[1] < 0):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        else:
-            self.__position = value
-
     def area(self):
-        """
-        area function
-        """
         return self.__size ** 2
 
     @property
@@ -58,22 +34,36 @@ class Square:
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
-            raise TypeError("size must be an integer")
+            raise TypeError('size must be an integer')
         elif value < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError('size must be >= 0')
         else:
             self.__size = value
 
     def my_print(self):
-        size = self.__size
-        if size == 0:
+        if self.__size == 0:
             print("")
         else:
-            for y in range(self.__position[1]):
+            for k in range(self.__position[1]):
                 print("")
-            for i in range(size):
-                for k in range(self.__position[0]):
+            for i in range(self.__size):
+                for x in range(self.__position[0]):
                     print(" ", end="")
-                for j in range(size):
+                for j in range(self.__size):
                     print("#", end='')
-                print("")
+                print('')
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple):
+            raise TypeError('position must be a tuple of 2 positive integers')
+        elif not isinstance(value[0], int):
+            raise TypeError('position must be a tuple of 2 positive integers')
+        elif not isinstance(value[1], int):
+            raise TypeError('position must be a tuple of 2 positive integers')
+        else:
+            self.__position = value
