ae49e4876c6e1b9a79d306729fd4501091386c25
Kroser-draid <kingdomskroser@gmail.com>
2024-07-11 04:20:36 +0100

    task 101

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-"""dds a new attribute"""
+"""
+function that add attributes
+"""
 
 
-def add_attribute(obj, attribute, value):
-    """add_attribute.
-
-    :param obj:
-    :param attribute:
-    :param value:
-    """
-    if '__dict__' not in dir(obj) or '__slots__' in dir(obj):
-        raise TypeError("can't add new attribute")
+def add_attribute(cls, attr, value):
+    if '__dict__' not in dir(cls) or '__slots__' in dir(cls):
+        raise TypeError('can\'t add new attribute')
     else:
-        setattr(obj, attribute, value)
+        setattr(cls, attr, value)
