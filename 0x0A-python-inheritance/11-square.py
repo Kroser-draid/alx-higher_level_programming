9e1d17efd556fd1593971fbc1c02b06030899220
Kroser-draid <kingdomskroser@gmail.com>
2024-07-11 03:39:59 +0100

    task 11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,20 +1,13 @@
 #!/usr/bin/python3
-"""class"""
+"""
+Module for Square class
+"""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Square."""
-
+    """ class square inherites from rectangle """
     def __init__(self, size):
-        """__init__.
-
-        :param size:
-        """
-        Rectangle.integer_validator(self, "size", size)
+        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
-
-    def __str__(self):
-        """__str__."""
-        return f"[Square] {self.__size}/{self.__size}"
