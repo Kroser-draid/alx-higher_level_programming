6f2d44bc9463627cf48927c410ed563b98921cda
Kroser-draid <kingdomskroser@gmail.com>
2024-07-11 01:41:18 +0100

    task 7

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-"""class"""
+"""
+module for class BaseGeometry
+"""
 
 
 class BaseGeometry:
-    """BaseGeometry."""
+    """
+    basegeometry class
+    """
+    def __init__(self):
+        pass
 
     def area(self):
-        """area."""
-        raise Exception("area() is not implemented")
+        raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
-        """integer_validator.
-
-        :param name: name.
-        :param value: value.
-        """
         if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise TypeError(f'{name} must be an integer')
+        elif value <= 0:
+            raise ValueError(f'{name} must be greater than 0')
