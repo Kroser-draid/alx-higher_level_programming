f6066b7db37ce8e91c4077b2610a223621e9f2e9
Kroser-draid <kingdomskroser@gmail.com>
2024-07-11 06:26:40 +0100

    task 0

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""function"""
+"""
+Module of a function that returns a representation of a json file
+"""
 import json
 
 
 def to_json_string(my_obj):
-    """to_json_string.
-
-    :param my_obj:
-    """
     return json.dumps(my_obj)
