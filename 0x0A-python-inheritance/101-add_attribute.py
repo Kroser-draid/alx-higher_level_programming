759fe85ea295162d238def0ba501a9d7474f74c4
Kroser-draid <kingdomskroser@gmail.com>
2024-07-11 04:21:51 +0100

    task 101

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -5,6 +5,7 @@ function that add attributes
 
 
 def add_attribute(cls, attr, value):
+    """ function that add a new attribute if it's possible to an object """
     if '__dict__' not in dir(cls) or '__slots__' in dir(cls):
         raise TypeError('can\'t add new attribute')
     else:
