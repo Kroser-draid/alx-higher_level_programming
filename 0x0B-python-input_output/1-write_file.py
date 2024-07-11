b95aa141fa72c6801204584c3ddadef7abbd4767
Kroser-draid <kingdomskroser@gmail.com>
2024-07-11 05:54:46 +0100

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,6 +5,7 @@ Module for a function that write in a file in utf-8 encoding
 
 
 def write_file(filename="", text=""):
+    """ function that write to a file """
     with open(filename, "w", encoding='UTF-8') as file:
         chars = file.write(text)
     return chars
