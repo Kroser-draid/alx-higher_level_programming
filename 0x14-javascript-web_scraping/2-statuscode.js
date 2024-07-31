2c33d6f6929cfd3f53cadb0ad8456a724577ab72
Kroser-draid <contact@kroserdraid.tech>
2024-07-31 02:24:37 +0100

    task 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const request = require('request');
+
+request(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.error('Error:', error.message);
+    return;
+  }
+
+  if (!response) {
+    console.error('No response received');
+    return;
+  }
+
+  console.log('code: ', response.statusCode);
+});
