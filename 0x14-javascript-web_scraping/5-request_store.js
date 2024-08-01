34bc11491809712c23bb438d561ea9a843453763
Kroser-draid <contact@kroserdraid.tech>
2024-08-01 02:53:36 +0100

    task 5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const url = process.argv[2];
+const path = process.argv[3];
+const request = require('request');
+const fs = require('fs');
+
+request(url, (err, response, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  } else if (response.statusCode ==! 200) {
+    console.log('Unexpected status code:', response.statusCode);
+    return;
+  }
+  fs.writeFileSync(path, body, 'utf-8');
+});
