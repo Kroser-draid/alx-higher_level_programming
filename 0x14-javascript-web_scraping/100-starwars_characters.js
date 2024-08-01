dfeadd208d8b64500c98aaa0bcbc8c2f79ca2ca7
Kroser-draid <contact@kroserdraid.tech>
2024-08-01 06:28:04 +0100

    fix 100

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -3,8 +3,7 @@
 const request = require('request');
 const movie = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2] + '/';
 
-
-function PrintChar(path) {
+function PrintChar (path) {
   request(path, (err, response, body) => {
     if (err) {
       console.error(err);
@@ -13,19 +12,19 @@ function PrintChar(path) {
       console.log('Unexpected status code:', response.statusCode);
     }
     const data = JSON.parse(body);
-    console.log((data['name']));
+    console.log((data.name));
   });
 }
 request(movie, (err, response, body) => {
-    if (err) {
-      console.error(err);
-      return;
-    } else if (response.statusCode !== 200) {
-      console.log('Unexpected status code:', response.statusCode);
-    }
-    const data = JSON.parse(body);
-    for (charIndex in data.characters) {
-      const charLink = data.characters[charIndex];
-      PrintChar(charLink);
-    }
-  });
+  if (err) {
+    console.error(err);
+    return;
+  } else if (response.statusCode !== 200) {
+    console.log('Unexpected status code:', response.statusCode);
+  }
+  const data = JSON.parse(body);
+  for (const charIndex in data.characters) {
+    const charLink = data.characters[charIndex];
+    PrintChar(charLink);
+  }
+});
