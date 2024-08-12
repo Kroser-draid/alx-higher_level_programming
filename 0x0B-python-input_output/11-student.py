6e54ccffdd817f0dca972db448f7583b5386cbab
Kroser-draid <contact@kroserdraid.tech>
2024-08-12 06:20:30 +0100

    task 11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,34 +1,34 @@
 #!/usr/bin/python3
-"""file"""
+""" module of a student clss """
 
 
 class Student:
-    """Student."""
-
+    """ student class """
     def __init__(self, first_name, last_name, age):
-        """__init__.
-
-        :param first_name:
-        :param last_name:
-        :param age:
-        """
+        """ init """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """to_json."""
-        if attrs is None:
-            return self.__dict__
+        """ return rep """
+
+        ser_obj = {}
+
+        if (attrs is None):
+            for k, v in self.__dict__.items():
+                if isinstance(v, (dict, list, str, int, bool)):
+                    ser_obj[k] = v
         else:
-            return {key: value for key, value in self.__dict__.items()
-                    if key in attrs}
+            for k, v in self.__dict__.items():
+                if (isinstance(v, (dict, list, str, int, bool))
+                        and k in attrs):
+                    ser_obj[k] = v
+
+        return ser_obj
 
     def reload_from_json(self, json):
-        """reload_from_json.
+        """ method that replaces the attrs """
 
-        :param json:
-        """
-        for key, value in json.items():
-            if hasattr(self, key):
-                setattr(self, key, value)
+        for k, v in json.items():
+            self.k = v
