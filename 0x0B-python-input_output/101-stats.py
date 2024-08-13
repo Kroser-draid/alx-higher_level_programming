d925ae25827996bbe9dff03c88c2df2054ae9021
Kroser-draid <contact@kroserdraid.tech>
2024-08-13 18:36:57 +0100

    task 101

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,36 +1,56 @@
 #!/usr/bin/python3
-"""file"""
-
+""" Module for function  """
 import sys
 
-if __name__ == "__main__":
-    total_file_size = 0
-    status_code_counts = {200: 0, 301: 0, 400: 0,
-                          401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-    line_count = 0
+while True:
+    ok = 0
+    moved = 0
+    bad = 0
+    unauth = 0
+    forbidden = 0
+    not_found = 0
+    not_supported = 0
+    server_error = 0
+    size = 0
 
-    try:
-        for line in sys.stdin:
-            parts = line.split()
-            if len(parts) >= 7:
-                status_code = int(parts[-2])
-                file_size = int(parts[-1])
+    for i in range(10):
 
-                total_file_size += file_size
-                if status_code in status_code_counts:
-                    status_code_counts[status_code] += 1
 
-                line_count += 1
+        line = sys.stdin.readline()
+        parts = line.strip().split(' ')
+        size += int(parts[-1])
 
-            if line_count % 10 == 0:
-                print(f"File size: {total_file_size}")
-                for code, count in sorted(status_code_counts.items()):
-                    if count > 0:
-                        print("{}: {}".format(code, count))
+        if str(200) in line:
+            ok += 1
+        elif str(301) in line:
+            moved += 1
+        elif str(400) in line:
+            bad += 1
+        elif str(401) in line:
+            unauth += 1
+        elif str(403) in line:
+            forbidden += 1
+        elif str(404) in line:
+            not_found += 1
+        elif str(405) in line:
+            not_supported += 1
+        elif str(500) in line:
+            server_error += 1
 
-    except KeyboardInterrupt:
-        raise
-    print(f"File size: {total_file_size}")
-    for code, count in sorted(status_code_counts.items()):
-        if count > 0:
-            print("{}: {}".format(code, count))
+    print("File size:", size)
+    if ok != 0:
+        print("200:", ok)
+    if moved != 0:
+        print("301:", moved)
+    if bad != 0:
+        print("400:", bad)
+    if unauth != 0:
+        print("401:", unauth)
+    if forbidden != 0:
+        print("403:", forbidden)
+    if not_found != 0:
+        print("404:", not_found)
+    if not_supported != 0:
+        print("405:", not_supported)
+    if server_error != 0:
+        print("500:", server_error)
