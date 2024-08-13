d6fabdfd1ff53971691ed1839d5a8195b79bf23d
Kroser-draid <contact@kroserdraid.tech>
2024-08-13 13:15:19 +0100

    task 12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,24 +1,18 @@
 #!/usr/bin/python3
-"""pascal triangle"""
+""" module for pascal triangle function """
 
 
 def pascal_triangle(n):
-    """pascal_triangle.
+    """ pascal triangle function returns a list of list """
+    result = [[1]]
 
-    :param n:
-    """
-    pascal = []
-    if n <= 0:
-        return pascal
-    pascal.append([1])
-    for i in range(1, n):
-        previous_row = pascal[i - 1]
-        new_row = [1]
-        for j in range(1, i):
-            new_element = previous_row[j - 1] + previous_row[j]
-            new_row.append(new_element)
+    for i in range(n):
+        if (i != 0):
+            result.append([])
+            for j in range(i + 1):
+                if j == 0 or j == i:
+                    result[i].append(1)
+                else:
+                    result[i].append(result[i - 1][j] + result[i - 1][j - 1])
 
-        new_row.append(1)
-        pascal.append(new_row)
-
-    return pascal
+    return result
